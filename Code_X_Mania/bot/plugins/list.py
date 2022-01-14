2ac5ddfe48dc45e31c025ed1c90c4dc1e67e51b3
Selfie SD <webtamil0@gmail.com>
2022-01-14 11:55:40 +0530

    Update list.py

--- Code_X_Mania/bot/plugins/list.py
+++ Code_X_Mania/bot/plugins/list.py
@@ -2,7 +2,7 @@ from Code_X_Mania.bot import StreamBot
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@StreamBot.on_message(filters.command("list"))
+@StreamBot.on_message(filters.command("LIST"))
 def _start(client, message):
     client.send_message(chat_id = message.chat.id,
         text = LIST_MSG.format(message.from_user.mention),
@@ -10,4 +10,4 @@ def _start(client, message):
         parse_mode="markdown"
     )
     
-    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1 . `start⚡️` \n 2. `help📚` \n 3. `login🔑` \n 4.`follow❤️` \n 5. `ping📡` \n 6. `status📊` \n 7. `dc` this tells your telegram dc \n 8. `maintainers😎` "
+    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1 . `START` \n 2. `HELP` \n 3. `LOGIN` \n 4.`FOLLOW` \n 5. `PING` \n 6. `STATUS` \n 7. `DC` this tells your telegram dc \n 8. `MAINTAINERS` "
