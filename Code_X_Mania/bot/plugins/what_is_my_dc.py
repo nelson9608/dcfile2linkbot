02a602e5f8b017352b302066ebdca54d299baef5
Selfie SD <webtamil0@gmail.com>
2022-01-14 11:51:22 +0530

    Update what_is_my_dc.py

--- Code_X_Mania/bot/plugins/what_is_my_dc.py
+++ Code_X_Mania/bot/plugins/what_is_my_dc.py
@@ -2,7 +2,7 @@ from Code_X_Mania.bot import StreamBot
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@StreamBot.on_message(filters.regex("dc"))
+@StreamBot.on_message(filters.regex("DC"))
 async def start(bot, update):
     text = START_TEXT.format(update.from_user.dc_id)
     await update.reply_text(
@@ -11,4 +11,4 @@ async def start(bot, update):
         quote=True
     )
 
-START_TEXT = """ Your Telegram DC Is : `{}`  """
+START_TEXT = """ 🏅 Your Telegram DC Is : `{}`  """
